889ae907d48f704e1d68f090c4ac2f706fe13614
LinusHolmer <linus.holmer@stud.sti.se>
2025-09-18 15:05:49 +0200

    fix: removed unused imports

--- src/main/java/se/johan/webservice_uppgift/dto/SentMessageDTO.java
+++ src/main/java/se/johan/webservice_uppgift/dto/SentMessageDTO.java
@@ -1,8 +1,5 @@
 package se.johan.webservice_uppgift.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 import java.time.LocalDateTime;
 
 public record SentMessageDTO(
